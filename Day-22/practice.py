1df48ef34a8c39a6376dd24978c953faabb29b2c
Lillian Parker <Lillianpparker@outlook.com>
2020-10-10 14:57:46 -0500

    day 22 and day 23

---
+++ Day-22/practice.py
@@ -0,0 +1,37 @@
+# Testing Python out
+# msg = "Hello World"
+# print(msg)
+
+# print("     /|")
+# print("    / |")
+# print("   /  |")
+# print("  /   |")
+# print(" /____|")
+
+# name= "Lillian"
+# status= " is awesome"
+
+# print(name.upper())
+# print(status.isupper())
+# print(name.upper() + status.upper())
+# print(len(name))
+# print(name[2])
+# print(status.index("a"))
+# print(status.index("is"))
+# print(status.replace("awesome", "boring"))
+
+#-------------------------------------------
+
+# name = input("Please enter your name: ")
+# print(name)
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter a second number: ")
+# result = int(num1) + float(num2)
+#******int looks for a whole number, NO DECIMALS ALOWED
+#******float allows for decimals
+# print(result)
+
+#-------------------------------------------
+
+animals = ["Reindeer", "Bengal Tiger", "Arctic Fox", "American Alligator"]
