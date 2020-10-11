af305e885743624b86479305ded0f155c2167388
Lillian Parker <Lillianpparker@outlook.com>
2020-10-11 13:54:30 -0500

    pushing

--- Day-22/practice.py
+++ Day-22/practice.py
@@ -34,4 +34,37 @@
 
 #-------------------------------------------
 
-animals = ["Reindeer", "Bengal Tiger", "Arctic Fox", "American Alligator"]
+# def sayhi(name, age):
+#     print ("Hello " + name + ", your age is: " + str(age))
+
+# sayhi("Lillian", 24)
+
+#-------------------------------------------
+
+# def cube(num):
+#     return num*num*num
+
+# result = cube(3)
+# print(result)
+
+#-------------------------------------------
+
+# is_tiger = True
+# is_pretty = True
+
+# if is_tiger & is_pretty:
+#     print("You are a purrty girl")
+# else:
+#     print("oof")
+
+#-------------------------------------------
+
+# def max_num(num1, num2, num3):
+#     if num1 >= num2 and num1 >= num3:
+#         return num1
+#     elif num2 >= num1 and num2 >= num3:
+#         return num2
+#     else:
+#         return num3
+
+# print(max_num(3, 4, 5))
